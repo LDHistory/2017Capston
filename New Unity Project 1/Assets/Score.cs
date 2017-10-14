e3b4b66856c7a26269f3b9caeff18a31778b57bf
jyhurs <32788960+jyhurs@users.noreply.github.com>
2017-10-14 22:01:49 +0900

    jyhurs

---
+++ New Unity Project 1/Assets/Score.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    public static float SCORE=1;
+    // Use this for initialization
+    void Start () {
+        SCORE =1f;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SCORE != 0)
+        {
+
+            SCORE += Time.deltaTime;
+        }
+            int t = Mathf.FloorToInt(SCORE);
+            Text uiText = GetComponent<Text>();
+            uiText.text = "점수: " + t.ToString();
+    }
+}
---
+++ New Unity Project 1/Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public static float time;
+	// Use this for initialization
+	void Start () {
+        time = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (time != 0) {
+
+            time += Time.deltaTime;
+
+        }
+        int t=Mathf.FloorToInt(time);
+       Text uiText=GetComponent< Text > ();
+        uiText.text = "경과 시간: " + t.ToString();
+	}
+}
